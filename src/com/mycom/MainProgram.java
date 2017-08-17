57641b8fe790a5a2790e0ee09d4f81439edfd287
Laxmikanthreddy Koduru <laxmikanthreddy.koduru@pearson.com>
2017-08-17 15:58:01 -0400

    added new service implementation

--- src/com/mycom/MainProgram.java
+++ src/com/mycom/MainProgram.java
@@ -1,5 +1,10 @@
 package com.mycom;
 
+import java.util.List;
+
+import com.mycom.soapservice.ProductCatalog;
+import com.mycom.soapservice.ProductCatalogImplService;
+
 import net.webservicex.*;
 
 public class MainProgram {
@@ -20,7 +25,46 @@ public class MainProgram {
 			GeoIP geoIP = ipServiceSoap.getGeoIP(ipAddress);
 			System.out.println("Location of given IP address "+ipAddress +" :"+geoIP.getCountryName());
 		}
+		
+		getProductCategories();
+		if(addProduct("books","My Novel"))
+		{
+			getProducts("books");
+		}
 
+	}
+	
+	public static void getProductCategories()
+	{
+		ProductCatalogImplService service = new ProductCatalogImplService();
+		ProductCatalog catalog = service.getProductCatalogImplPort();
+		List<String> categories = catalog.getProductCategories();
+		System.out.println("Print Categories");
+		for(int i = 0;i<categories.size();i++)
+		{
+			System.out.println(categories.get(i));
+			getProducts(categories.get(i));
+		}
+		
+	}
+	
+	public static void getProducts(String category)
+	{
+		ProductCatalogImplService service = new ProductCatalogImplService();
+		ProductCatalog catalog = service.getProductCatalogImplPort();
+		List<String> products = catalog.getProducts(category);
+		System.out.println("Print Products");
+		for(int i = 0;i<products.size();i++)
+		{
+			System.out.println(products.get(i));
+		}
+	}
+	
+	public static boolean addProduct(String category, String product)
+	{
+		ProductCatalogImplService service = new ProductCatalogImplService();
+		ProductCatalog catalog = service.getProductCatalogImplPort();
+		return catalog.addProduct(category, product);
 	}
 
 }
---
+++ src/com/mycom/soapservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://soapservice.mycom.com/")
+package com.mycom.soapservice;
